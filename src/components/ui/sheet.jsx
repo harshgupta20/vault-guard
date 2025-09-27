6e2962e614f2559edb2628a384cf2b4c806924b6
TheRealRajdeep <rajdeepjadeja2806@gmail.com>
2025-09-27 11:33:44 +0530

    [+] Hamburger menu animation

--- src/components/ui/sheet.jsx
+++ src/components/ui/sheet.jsx
@@ -16,7 +16,7 @@ const SheetPortal = SheetPrimitive.Portal
 const SheetOverlay = React.forwardRef(({ className, ...props }, ref) => (
   <SheetPrimitive.Overlay
     className={cn(
-      "fixed inset-0 z-50 bg-background/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+      "fixed inset-0 z-50 bg-background/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:duration-200 data-[state=open]:duration-300",
       className
     )}
     {...props}
@@ -26,11 +26,11 @@ const SheetOverlay = React.forwardRef(({ className, ...props }, ref) => (
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName
 
 const sheetVariants = cva(
-  "fixed z-50 gap-4 bg-background p-6 shadow-lg transition-all ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
+  "fixed z-50 gap-4 bg-background p-6 shadow-lg transition-all ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-200 data-[state=open]:duration-300",
   {
     variants: {
       side: {
-        top: "inset-x-0 top-20 ml-4 mr-4 border-b rounded-b-md data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top data-[state=open]:animate-[slideInFromTop_0.2s_cubic-bezier(0.4,0,0.2,1)] data-[state=closed]:animate-[slideOutToTop_0.15s_cubic-bezier(0.4,0,0.2,1)]",
+        top: "inset-x-0 top-20 ml-4 mr-4 border-b rounded-b-md data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top data-[state=open]:animate-[slideInFromTop_0.3s_cubic-bezier(0.16,1,0.3,1)] data-[state=closed]:animate-[slideOutToTop_0.2s_cubic-bezier(0.4,0,1,1)]",
         bottom:
           "inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
         left: "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm",
